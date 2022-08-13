974e116b003015258346f615c997f94a92fb1841
John Dunn <alch3myst@gmail.com>
2022-08-13 12:16:13 -0500

    comment form

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,6 +1,21 @@
 const CommentForm = () => {
   return (
-    <div><h4>CommentForm</h4>
+    <div className="comment-form">
+      <form onSubmit={submitComment}>
+        <label htmlFor="name">Description:</label>
+        <textarea
+          type="text"
+          id="newComment"
+          name="name"
+          placeholder={comment.description}
+          value={newComment.description}
+          onInput={newComment}
+        />
+        <button
+          type="submit">
+          Send
+        </button>
+      </form>
     </div>
   )
 }
