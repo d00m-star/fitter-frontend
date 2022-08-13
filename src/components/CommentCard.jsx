e0682c94e5874405f7f5f839fe793c882ed35236
John Dunn <alch3myst@gmail.com>
2022-08-13 11:18:12 -0500

    comment card setup

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,24 +1,15 @@
-// import React from 'react'
 
-// const CommentCard = () => {
-//   return ( <div className='comment-list'>
-//     { comments.map((comment) => (
-//       <div className='comment-card' onClick={() => showComment(comment)}  key={comment.id}
-//         <h4>{this.name}</h4>
-//         <h4>{this.age}</h4>
-//         <h2>{comment.description}</h2>
-//       </div>
-//       )
-      
-//     )}
-//   </div>
-//   )
-// }
-
-// export default CommentCard
-
-// comments.map((comment) => {
-//   <div className="comment-card" onClick={() => showComment(comment)} key={comment.id}>
-//     <h4>{this.name}</h4>
-//     <h4>{this.age}</h4>
-//     <h3>{comment.description}</h3>
+const CommentCard = () => {
+  return ( <div className='comment-list'>
+    { comments.map((comment) => (
+      <div className='comment-card' key={comment.id}>
+        <h4>{comment.username}</h4>
+        <h2>{comment.description}</h2>
+        <button onClick={() => showComment(comment)}>Update</button>
+      </div>
+      ) 
+    )}
+  </div>
+  )
+}
+export default CommentCard
