95435141aa089e90240342187ce37d3f53c7b0f6
Steve Morello <smorello4@gmail.com>
2022-08-14 23:11:31 -0400

    add home.css, attempt card flip on home

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,33 +1,31 @@
 const LoginForm = ({ loginValues, updateLoginValues, submitLogin }) => {
   return (
-    <div>
-      <form onSubmit={(e) => submitLogin(e)}>
-        <label htmlFor="login-username">Username</label>
-        <input
-          type="text"
-          id="login-username"
-          name="username"
-          value={loginValues.username}
-          onInput={updateLoginValues}
-          required
-        />
-        <label htmlFor="login-password">Password</label>
-        <input
-          type="password"
-          id="login-password"
-          name="password"
-          value={loginValues.password}
-          onInput={updateLoginValues}
-          required
-        />
-        <button
-          type="submit"
-          disabled={!loginValues.username || !loginValues.password}
-        >
-          Login
-        </button>
-      </form>
-    </div>
+    <form onSubmit={(e) => submitLogin(e)} id="login-form">
+      <label htmlFor="login-username">Username</label>
+      <input
+        type="text"
+        id="login-username"
+        name="username"
+        value={loginValues.username}
+        onInput={updateLoginValues}
+        required
+      />
+      <label htmlFor="login-password">Password</label>
+      <input
+        type="password"
+        id="login-password"
+        name="password"
+        value={loginValues.password}
+        onInput={updateLoginValues}
+        required
+      />
+      <button
+        type="submit"
+        disabled={!loginValues.username || !loginValues.password}
+      >
+        Login
+      </button>
+    </form>
   )
 }
 
